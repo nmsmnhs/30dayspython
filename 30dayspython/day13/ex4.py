3160bf50b7de8e510406487dbde70dfbf9cc938f
nmsmnhs <phuongquynh.pham1202@gmail.com>
2025-04-10 21:31:18 +0700

    Add files via upload

---
+++ 30dayspython/day13/ex4.py
@@ -0,0 +1,9 @@
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+three = lambda x : x[0:3]
+output = [list(row) for block in countries for row in block]
+for row in output:
+    row.insert(1, three(row[0]))
+    for i in range(3):
+        row[i] = row[i].upper()
+
+print(output)
